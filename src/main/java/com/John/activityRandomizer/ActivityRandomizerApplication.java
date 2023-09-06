64e694acb119439a1375ece5fff848968165b6fd
unknown <ibejenariu98@yahoo.com>
2023-09-06 19:28:23 +0300

    First commit

---
+++ src/main/java/com/John/activityRandomizer/ActivityRandomizerApplication.java
@@ -0,0 +1,13 @@
+package com.John.activityRandomizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivityRandomizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivityRandomizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/John/activityRandomizer/ActivityRandomizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.John.activityRandomizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivityRandomizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
